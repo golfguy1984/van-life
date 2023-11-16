fcbc55f855fc5991b0b6c031cab4d10d582b960f
golfguy1984 <stephenrwalters@gmail.com>
2023-11-15 21:11:20 -0500

    css changes

--- Host/HostIncome.jsx
+++ Host/HostIncome.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 function HostIncome() {
+
   return (
-    <div>HostIncome</div>
+    <>
+      <div className='host-income-top'>
+        <h3>Income</h3>
+        <p>last 30 days</p>
+        <h2>$2,260</h2>
+      </div>
+      <div>
+        Chart goes here
+      </div>
+      <div>
+        <h3>Your Transactions(3)</h3>
+        <p>Last 30 days</p>
+      </div>
+      <>List goes here</>
+    </>
   )
 }
 
