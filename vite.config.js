9ec3c952f0ca74e425c08993decb172bad757a08
golfguy1984 <stephenrwalters@gmail.com>
2024-01-04 16:50:35 -0500

    update mkcert

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"; // Ensure that you have this import statement
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   server: {
