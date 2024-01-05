2e62186aa983fe25f18ea475ef9437fdc74a21bb
golfguy1984 <stephenrwalters@gmail.com>
2024-01-05 10:06:00 -0500

    netlify functions updates

--- backend/netlify/functions/get-state.js
+++ backend/netlify/functions/get-state.js
@@ -1,15 +1,23 @@
-import express from "express";
+import express, { Router } from "express";
 import serverless from "serverless-http";
 import axios from "axios";
+import dotenv from "dotenv";
 
 const app = express();
+app.use(express.json());
+dotenv.config();
 
-app.post("/", async (req, res) => {
+const router = Router();
+
+router.post("/get-state", async (req, res) => {
   try {
+    console.log(process.env.GOOGLE_MAPS_API_KEY);
     const { latitude, longitude } = req.body;
+
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.GOOGLE_MAPS_API_KEY}`
     );
+    console.log(response);
     if (response.data.results.length > 0) {
       const state = response.data.results[0]?.address_components.find(
         (component) => component.types.includes("locality")
@@ -27,5 +35,7 @@ app.post("/", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+app.use("/get-state/", router);
 
 export const handler = serverless(app);
--- backend/netlify/functions/test.js
+++ backend/netlify/functions/test.js
@@ -4,14 +4,8 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World Again!"));
-
 router.get("/test", (req, res) => res.send("test!"));
 
-router.post("/get-state", (req, res) => {
-  res.json({ message: "Received POST request to get-state" });
-});
-
-api.use("/api/", router);
+api.use("/test/", router);
 
 export const handler = serverless(api);
