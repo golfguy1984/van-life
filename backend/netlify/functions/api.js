7a10d891224d78806302c19eacb7642fea2c54fe
golfguy1984 <stephenrwalters@gmail.com>
2024-01-04 22:08:16 -0500

    test netllify functions

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -6,6 +6,30 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/", router);
+router.post("/get-state", async (req, res) => {
+  try {
+    const { latitude, longitude } = req.body;
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+    );
+    if (response.data.results.length > 0) {
+      const state = response.data.results[0]?.address_components.find(
+        (component) => component.types.includes("locality")
+      );
+      if (state) {
+        res.json({ state: state.long_name });
+      } else {
+        res.status(404).json({ error: "State not found in the response" });
+      }
+    } else {
+      res.status(404).json({ error: "No results found" });
+    }
+  } catch (error) {
+    console.error("Error getting state:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+api.use("/.netlify/functions/", router);
 
 export const handler = serverless(api);
